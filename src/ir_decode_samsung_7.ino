a96781e451e0bade2394494179d804ec162181d9
Marcos <marcosscheeren@gmail.com>
2017-03-10 17:39:06 -0300

    Commit before change decode approach
    Code was acting weird due to bad usage of interrupts on main loop (i think)

--- src/ir_decode_samsung_7.ino
+++ src/ir_decode_samsung_7.ino
@@ -1,17 +1,19 @@
+#include <Arduino.h>
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
 #define TIMER_RESET TCNT1 = 0
-#define SAMPLE_SIZE 128
+#define SAMPLE_SIZE 150
 
 int IRpin = 11;
 unsigned int TimerValue[3][SAMPLE_SIZE];
 char direction[3][SAMPLE_SIZE];
 byte change_count;
 long time;
-long curr_tcnt1 = 0;
-long prev_tcnt1 = 0;
+unsigned long curr_tcnt1 = 0;
+unsigned long prev_tcnt1 = 0;
+unsigned long ellapsed = 0;
 int chunk = 0;
 
 void setup() {
@@ -32,41 +34,76 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Waiting...");
+  // reset everything
   change_count = 0;
+  chunk = 0;
+  memset(TimerValue, 0, sizeof(TimerValue));
+
+  Serial.println("Waiting2...");
+
   while (digitalRead(IRpin) == HIGH) {
   }
   TIMER_RESET;
   TimerValue[chunk][change_count] = TCNT1;
+  ellapsed = 0;
   int init_count = change_count++;
   int chunk_index;
   direction[0][init_count] = '0';
   direction[1][init_count] = '0';
   direction[2][init_count] = '0';
 
-  while (chunk <= 3) {
+  while (chunk < 3) {
     //    while (change_count < SAMPLE_SIZE) {
     curr_tcnt1 = TCNT1;
 
-    if (curr_tcnt1 - prev_tcnt1 > 2000) {
-      Serial.println(curr_tcnt1 - prev_tcnt1);
+    if (prev_tcnt1 > curr_tcnt1) {
+      ellapsed = ellapsed + (65535 - prev_tcnt1) + curr_tcnt1; // rollover
+    } else {
+      //      Serial.print("\n<");
+      ellapsed = ellapsed + (curr_tcnt1 - ellapsed);
+    }
 
+    //    if (ellapsed > 100000) {
+    //      break;
+    //    }
+
+    if (curr_tcnt1 - prev_tcnt1 > 2000 && chunk <= 3) {
       chunk++;
       chunk_index = chunk - 1;
       change_count = init_count;
       Serial.println(chunk);
-      Serial.println(chunk_index);
+      //      Serial.println(chunk_index);
     }
 
     if (direction[chunk_index][change_count - 1] == '0') {
       prev_tcnt1 = TCNT1;
       while (digitalRead(IRpin) == LOW) {
+        if (ellapsed + TCNT1 > 75000) {
+          chunk++;
+          //          break;
+        }
+        //        if (ellapsed + TCNT1 > 100000) {
+        //          Serial.println("\ntimeout");
+        //          Serial.println(ellapsed + TCNT1 );
+        //          chunk = 4;
+        //          break;
+        //      }
       }
       TimerValue[chunk_index][change_count] = TCNT1 - prev_tcnt1;
       direction[chunk_index][change_count++] = '1';
     } else {
       prev_tcnt1 = TCNT1;
       while (digitalRead(IRpin) == HIGH) {
+        if (ellapsed + TCNT1 > 75000) {
+          chunk++;
+          //          break;
+        }
+        //        if (ellapsed + TCNT1 > 100000) {
+        //          Serial.println("\ntimeout");
+        //          Serial.println(ellapsed + TCNT1 );
+        //          chunk = 4;
+        //          break;
+        //      }
       }
       TimerValue[chunk_index][change_count] = TCNT1 - prev_tcnt1;
       direction[chunk_index][change_count++] = '0';
@@ -76,25 +113,69 @@ void loop() {
 
   Serial.println("Bit stream detected!");
   chunk = 0;
-
+  // WORKS
+  byte cycle[7] = {0};
+  int cycletime;
+  int shifter;
+  int byte_index = 0;
+  change_count = 0;
   while (chunk < 3) {
     time = (long)TimerValue[chunk][change_count] * 4;
-    Serial.print(time);
-    Serial.print("\t");
-    Serial.println(direction[chunk][change_count++]);
+    //    time = (long) TimerValue[chunk][change_count];
+    //    Serial.print(time);
+    //    Serial.print("\t");
+    //    Serial.println(direction[chunk][change_count++]);
+
     while (change_count < SAMPLE_SIZE) {
-      time = (long)TimerValue[chunk][change_count] * 4;
-      //   Serial.print(time);
-      //   Serial.print("\t");
-      //   Serial.println(direction[change_count-1]);
-      Serial.print(time);
+      Serial.print("\n cc:");
+      Serial.print(change_count);
+      cycletime =
+          TimerValue[chunk][change_count] + TimerValue[chunk][change_count + 1];
+      if (TimerValue[chunk][change_count] > 100 &&
+          TimerValue[chunk][change_count] < 500) { // check for invalid timings
+        byte_index = ((change_count + 14) / 16) - 1;
+        shifter = (byte_index + 1) * 8 - (change_count / 2);
+        //        shifter = 8 - ((change_count - 1) / 2);
+        //        Serial.print("\t byte_index: ");
+        //        Serial.print(byte_index);
+        if (cycletime < 300) {
+          Serial.print("\t decoded: ");
+          Serial.print("1");
+          cycle[byte_index] = cycle[byte_index] | (0x1 << shifter);
+        } else {
+          Serial.print("\t decoded: ");
+          Serial.print("0");
+          cycle[byte_index] = cycle[byte_index] | (0x0 << shifter);
+        }
+        Serial.print("\t shifted: ");
+        Serial.print(shifter);
+      }
+
+      //            Serial.print("\t");
+      //      Serial.print("0x");
+      //      Serial.print(cycle[byte_index], HEX);
       Serial.print("\t");
-      Serial.println(direction[chunk][change_count++]);
+      Serial.print(cycletime);
+      //      Serial.print("\t");
+      //            Serial.print((long) TimerValue[chunk][change_count]);
+      //            Serial.print(" : ");
+      //            Serial.print((long) TimerValue[chunk][change_count - 1]);
+      //            Serial.println("\t");
+      change_count++;
+      //      Serial.print(direction[chunk][change_count]);
+      change_count++; // odd check
     }
+    Serial.print("\nFinal byte: 0x");
+    for (int i = 0; i < sizeof(cycle); i++) {
+      Serial.print(cycle[i], HEX);
+      Serial.print(" ");
+      cycle[i] = (char)0;
+    }
+
     chunk++;
     change_count = 0;
   }
 
-  Serial.println("Bit stream end!");
+  Serial.println("\nBit stream end!");
   delay(2000);
 }
